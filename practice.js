07eab90584ecae48bfcd81d524acdf8040461949
basyobasyo <om1997518@gmail.com>
2022-03-07 09:13:28 +0900

    Update practice.js

--- practice.js
+++ practice.js
@@ -55,7 +55,72 @@ const practice = () => {
   delete y;
   console.log(y);
 
-  
+  // 第１１問
+  // let a11 =[
+  //   {id:1,name:'morita'},
+  //   {id:2,name:'kenji'},
+  //   {id:4,name:'uro'},
+  //   {id:3,name:'ken'}
+  //   ];
+
+  // a11.sort(function(a, b){
+  //   return a.id > b.id;
+  // });
+  // console.log(a11);
+
+  let arry =[
+    {id:1,name:'morita'},
+    {id:2,name:'kenji'},
+    {id:4,name:'uro'},
+    {id:3,name:'ken'}
+  ];
+  console.log(
+    arry.sort(function(a,b){
+    return  b.id - a.id // ここは比較をさせ、正の値になるので、bに入っていた要素が若いインデックス番号になる
+    })
+  );
+  console.log(arry); // sortメソッドは元の配列にも影響を与える
+
+  // 第１２問
+  let a12 = 5;
+  let b12 = 7;
+
+  a12 = 1;
+  console.log(a12, b12);
+
+  // 第１３問
+  // const next = () => {
+  //   let a13 = 0;
+  //   return function(){
+  //     return a13 += 1 ;
+  //   }
+  // }; // この関数をそのまま返してしまうと、 a13 += 1を返すことになるため、一度定数にこの値を入れる必要がある
+
+  const setUp = function(){
+    let count = 0;
+    return function(){
+  return (count += 1);
+    }
+  };
+  const next = setUp();
+
+  console.log(next());
+  console.log(next());
+  console.log(next());
+
+  // 第１４問
+  // const fun = (a, b, c) => {
+  //   return [a, b, c];
+  // }
+  // const fun = (...arg) => {
+  //   return arg;
+  // }
+  function fun(){
+    return arguments[0]; // argumentsはJS内で使える配列のようなオブジェクト
+   }
+   // 上記３つであればどれでも可能
+
+  console.log(fun(1,2,3));
 
   
 }
